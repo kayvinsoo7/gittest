2782a1ce5855a2469f4051484b005fbc9b4a86c7
kayvinsoo7 <61403133+kayvinsoo7@users.noreply.github.com>
2022-05-19 08:24:37 +0200

    Update

--- mainRoutes.js
+++ mainRoutes.js
@@ -2,7 +2,7 @@ const path = require('path')
 const express = require('express')
 const mainRouter = express.Router()
 mainRouter.get('/', function (req, res) {
-res.send('Hello World-checkk')
+res.send('Hello World-check111')
 })
 mainRouter.get('/about', function (req, res) {
 res.sendFile(path.join(__dirname, 'views', 'about.html'))
